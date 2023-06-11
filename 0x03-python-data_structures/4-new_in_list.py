1440121a2cbbebbbe284a3981569bd2f03938b81
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-06-11 15:56:07 +0000

    revvert copying style

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
-    new_list = [x for x in my_list]git
+    new_list = my_list[:]
     new_list[idx] = element
     return (new_list)
 
