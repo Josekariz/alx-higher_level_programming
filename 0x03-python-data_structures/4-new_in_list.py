cdc3673e2cffe9ad1a7ba953bcd5a1ddf9db6afe
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-06-11 15:48:53 +0000

    fix range logic

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,7 @@
 def new_in_list(my_list, idx, element):
     """Replaces an element from a copy of the list at a certain index."""
     if idx in range(len(my_list)):
-        new_list= my_list[:]
-        new_list[idx] = element
-    return (my_list)
+        return my_list
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
