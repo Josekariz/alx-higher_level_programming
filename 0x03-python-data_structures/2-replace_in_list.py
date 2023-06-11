fdeb832e307427c9ddc272a64502c76bbadc5955
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-06-11 13:29:37 +0000

    edit if statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     """Replaces an element from a list at a certain index."""
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Return a copy of the original list to avoid modifying the original
-    my_list[idx] = element
-    return my_list
+    if idx in range(len(my_list)):
+        my_list[idx] = element
+    return (my_list)
