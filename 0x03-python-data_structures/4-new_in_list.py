ab51fd5ffecdc07b91dac11fa0c95d1595c9712a
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-06-11 15:53:18 +0000

    change if condition logic

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Replaces an element from a copy of the list at a certain index."""
-    if idx in range(len(my_list)):
-        return my_list
+    """Replace an element in a copied list at a specific index."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
     new_list = my_list[:]
     new_list[idx] = element
-    return new_list
+    return (new_list)
+
