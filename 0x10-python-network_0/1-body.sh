11ab2dc8cdf155a429b9a51b5c4a197888904775
Joseph <sejokarizz@gmail.com>
2023-11-30 20:38:17 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request, and displays the body of the response for a 200 status code.
-curl -sL -w "%{http_code}" "$1" -o /dev/null | [ "$(tail -c 3)" == "200" ] && curl -s "$1"
+curl -sL "$1"
